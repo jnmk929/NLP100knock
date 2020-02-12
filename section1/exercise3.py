6eaec6d3aef39df7f8408f24f17ed4ac1a4af2f5
jnmk929 <amour1125.j.0609@gmail.com>
2020-02-13 01:55:47 +0900

    03 単語分割とカウント

---
+++ section1/exercise3.py
@@ -0,0 +1,12 @@
+#03. 円周率
+#"Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+# という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+list = str.split(' ')
+result = []
+for word in list:
+    word = word.strip(",.")
+    result.append(len(word))
+print(result)
+
