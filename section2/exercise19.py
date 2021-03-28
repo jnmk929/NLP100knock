9bcfe08a203274c3d8439412e968659e006514ec
Junki <amour1125.j.0609@gmail.com>
2021-03-28 20:40:06 +0900

    19 カウントにcollections.counterを使用

--- section2/exercise19.py
+++ section2/exercise19.py
@@ -1,7 +1,8 @@
 #19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる
 #各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．
 # 確認にはcut, uniq, sortコマンドを用いよ．
-from itertools import groupby
+#from itertools import groupby
+import collections
 
 rfile = './hightemp.txt'
 lines = []
@@ -11,15 +12,17 @@ with open(rfile,mode='r',encoding='utf_8') as f:
        lines.append(line.split('\t')[0])
 
 # 都道府県で集計し、(都道府県, 出現頻度)のリスト作成
-lines.sort()
-result = [(line,len(list(group))) for line, group in groupby(lines)]
+#lines.sort()
+#result = [(line,len(list(group))) for line, group in groupby(lines)]
 # 出現頻度でソート
-result.sort(key=lambda line: line[1],reverse=True)
+#result.sort(key=lambda line: line[1],reverse=True)
+#for line in result:
+#    print(line[0],':',line[1])
 
-for line in result:
-    print(line[0],':',line[1])
-
-#わからん
+result = collections.Counter(lines)
+result = result.most_common()
+for key,value in result:
+    print(key,value)
 
 '''
 #!/bin/sh
