031d4f946fab2a74ae1adec75d7d85a937fa3626
Junki <amour1125.j.0609@gmail.com>
2021-03-28 16:20:19 +0900

    05 n_gramの関数追加

--- section1/exercise5.py
+++ section1/exercise5.py
@@ -11,6 +11,24 @@ def n_gram(list,n):
         gram.append(list[i:i+n])    
     return gram
 
+def n_gram2(string,select,n):
+    #単語n-gram
+    gram = []
+    if select == 0:
+        word = string.split(' ')
+        for i in range(len(word)-n+1):
+            gram.append(word[i:i+n])
+    #文字n-gram
+    if select == 1:
+        for i in range(len(string)-n+1):
+            gram.append(string[i:i+n])
+
+    return gram
+
+
+
 if __name__ == "__main__":
-    print(n_gram(str,2))
-    print(n_gram(word,2))
+    #print(n_gram(str,2))
+    #print(n_gram(word,2))
+    print(n_gram2(str,0,2))
+    print(n_gram2(str,1,2))
